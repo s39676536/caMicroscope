15d0c2acd5ad80a3644b73b7f242f07d6e8555ea
Erich Bremer <erich@ebremer.com>
2018-11-29 12:02:50 -0500

    Change meta data retrieveal to PathDB

--- js/imagemetadatatools/osdImageMetadata.js
+++ js/imagemetadatatools/osdImageMetadata.js
@@ -11,8 +11,25 @@ var OSDImageMetaData = new Class({
     },
     retrieveImageSize: function () {
         var jsonRequest = new Request.JSON({
+            url: "/node/"+this.imageId+"?_format=json", async: false, onSuccess: function (e) {
+                var yah = {};
+                yah["mpp-x"] = e.field_mpp_x[0].value;
+                yah["mpp-y"] = e.field_mpp_y[0].value;
+                var pro = [];
+                pro.push(yah);
+                pro.push(e.field_iip_path[0].value+".dzi");
+                console.log("erich : "+JSON.stringify(pro));
+                this.metaData = pro;
+            }.bind(this), onFailure: function (e) {
+                alert("Failed to get dimension");
+            }.bind(this)
+        }).get();
+    },
+    oldretrieveImageSize: function () {
+        var jsonRequest = new Request.JSON({
             url: "api/Data/osdMetadataRetriever.php", async: false, onSuccess: function (e) {
                 this.metaData = e;
+                console.log("erich meta "+e);
             }.bind(this), onFailure: function (e) {
                 alert("Failed to get dimension");
             }.bind(this)
